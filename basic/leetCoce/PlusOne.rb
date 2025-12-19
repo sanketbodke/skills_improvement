477b5ab3824f6609aadfe4f7f1319856e43a5988
sanketbodke <sanketbodke26@gmail.com>
2025-12-19 21:58:45 +0530

    Add implementation for checking if a string is a palindrome

---
+++ basic/leetCoce/PlusOne.rb
@@ -0,0 +1,72 @@
+# @param {Integer[]} digits
+# @return {Integer[]}
+
+# Example 1:
+
+# Input: digits = [1,2,3]
+# Output: [1,2,4]
+# Explanation: The array represents the integer 123.
+# Incrementing by one gives 123 + 1 = 124.
+# Thus, the result should be [1,2,4].
+# Example 2:
+
+# Input: digits = [4,3,2,1]
+# Output: [4,3,2,2]
+# Explanation: The array represents the integer 4321.
+# Incrementing by one gives 4321 + 1 = 4322.
+# Thus, the result should be [4,3,2,2].
+# Example 3:
+
+# Input: digits = [9]
+# Output: [1,0]
+# Explanation: The array represents the integer 9.
+# Incrementing by one gives 9 + 1 = 10.
+# Thus, the result should be [1,0].
+
+def plus_one(digits)
+  digits_into_array =  digits
+
+  last_val = (digits_into_array.length - 1)
+
+  updated_last_value = digits_into_array[last_val] + 1
+
+  if digits_into_array[last_val] == 9
+    digits_into_array.pop
+    digits_into_array << 1
+    digits_into_array << 0
+  else
+    digits_into_array.pop
+    digits_into_array.push(updated_last_value)  
+  end  
+
+  digits_into_array.inspect
+end
+
+puts plus_one(123)
+
+
+
+# @param {Integer[]} digits
+# @return {Integer[]}
+
+# Example 1:
+
+# Input: digits = [1,2,3]
+# Output: [1,2,4]
+# Explanation: The array represents the integer 123.
+# Incrementing by one gives 123 + 1 = 124.
+# Thus, the result should be [1,2,4].
+# Example 2:
+
+# Input: digits = [4,3,2,1]
+# Output: [4,3,2,2]
+# Explanation: The array represents the integer 4321.
+# Incrementing by one gives 4321 + 1 = 4322.
+# Thus, the result should be [4,3,2,2].
+# Example 3:
+
+# Input: digits = [9]
+# Output: [1,0]
+# Explanation: The array represents the integer 9.
+# Incrementing by one gives 9 + 1 = 10.
+# Thus, the result should be [1,0].
